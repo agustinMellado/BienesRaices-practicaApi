f2f3bda44e21837abc592a9230726741d8748f8d
Agustin Mellado <aguxmellado@gmail.com>
2023-01-10 10:56:01 -0300

    se creo routing, definio el puerto y utilizo express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hola mundo');
+const express = require('express')
+
+//Crear la app
+const app = express()
+
+// Routing
+app.get('/', function (req, res) {
+    res.res('Hola mundo en express')
+});
+
+
+//definir un puerto y arrancar el proyecto.
+const port = 3000;
+//conecto la app al puerto.
+app.listen(port, () => {
+    console.log(`el servidor esta funcionando en el puerto ${port}`)
+});
