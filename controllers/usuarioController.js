18cf83383198a34abf24cdeb045710e69de475f3
Agustin Mellado <aguxmellado@gmail.com>
2023-01-12 18:05:07 -0300

    modificaciones

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,7 +2,12 @@
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        autenticado: false
+    })
+}
+
+const formularioRegistro = (req, res) => {
+    res.render('auth/registro', {
+        
     })
 }
 
