2875d556b90babf73b597e8a061155b0251b60ab
Agustin Mellado <aguxmellado@gmail.com>
2023-01-25 08:14:42 -0300

    configuracion de la base de datos y sus conexiones

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import Sequelize  from "sequelize";
+
+const db = new Sequelize('bienesraices_node_mvc', 'root','',{
+    host: 'localhost',
+    port: 3306, //Puerto por defecto para mysql
+    dialect: 'mysql',
+    define: {
+        timestamp:true
+    },
+    //Configura como sera el comportamiento para conexiones nuevas o existentes.
+    pool: {
+        max: 5,//Maximo de conexiones a mantener.
+        min: 0,//Minimo, si no hay nada tratara de desconectar todo para liberar recursos.
+        acquire:30000, // Lo que tardara en elavorar una conexion o marcar un ERROR.
+        idle:10000 // Lo que tarda en finalizar la conexion sin movimiento.
+    },
+    operatorAliases: false
+
+});
+export default db;
