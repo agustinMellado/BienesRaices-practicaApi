36e16676dc496c86c9a67ccb1bdbeeea757f3d54
Agustin Mellado <aguxmellado@gmail.com>
2023-01-25 16:28:33 -0300

    declaracion de modelo usuarios

---
+++ models/Usuario.js
@@ -0,0 +1,7 @@
+import { DataTypes } from "sequelize";
+import db from '../config/db.js';
+
+//definimos la tabla que vamos a crear, con su determinada estructura.
+const Usuario = db.define('usuarios' ,{ 
+    //diferentes columnas de la tabla.
+})
