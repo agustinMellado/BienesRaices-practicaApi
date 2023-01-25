262e1a7faed1417b1b66918d63f5635993ee1a9c
Agustin Mellado <aguxmellado@gmail.com>
2023-01-25 16:37:04 -0300

    se agregaron las columnas necesaria para los campos del formulario

--- models/Usuario.js
+++ models/Usuario.js
@@ -3,5 +3,24 @@ import db from '../config/db.js';
 
 //definimos la tabla que vamos a crear, con su determinada estructura.
 const Usuario = db.define('usuarios' ,{ 
+
     //diferentes columnas de la tabla.
+    nombre:{
+        type: DataTypes.STRING, //TIPO DE DATO
+        allowNull: false //es para que el campo no quede vacio.
+    },
+    email:{
+        type: DataTypes.STRING, //TIPO DE DATO
+        allowNull: false //es para que el campo no quede vacio.
+    },
+    password:{
+        type: DataTypes.STRING, //TIPO DE DATO
+        allowNull: false //es para que el campo no quede vacio.
+    },
+    token: DataTypes.STRING, //TIPO DE DATO
+    confirmado: DataTypes.BOOLEAN
+
+    //NOTA: Al tener multiples atributos va llave, al tener uno solo no es necesario.
 })
+
+export default Usuario
