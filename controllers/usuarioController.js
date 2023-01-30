80d3433b401b3a6f8f409d75a0d171d8939200c0
Agustin Mellado <aguxmellado@gmail.com>
2023-01-30 18:39:13 -0300

    validacion al resto de los campos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -17,17 +17,20 @@ const formularioRegistro = (req, res) => {
     })
 }
 
-const registrar= async (req, res) => {
+const registrar = async (req, res) => {
     //validacion
     await check('nombre').notEmpty().withMessage('El nombre no puede ir vacio').run(req)//para que los campos no esten vacios.
-    
-    let resultado =validationResult(req)
+    await check('email').isEmail().withMessage('Ingrese un email valido').run(req)//que sean emails validos.
+    await check('password').isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres').run(req)//un minimo necesario de contrasenias
+    await check('repetir_password').equals('password').withMessage('Las contraseñas no coinciden').run(req)// verifica que las contrasenias coincidan.
+
+    let resultado = validationResult(req)
 
     res.json(resultado.array());
     //para leer la informacion de un form 'req.body'.
-    const usuario= await Usuario.create(req.body)//Crea un nuevo usuario con la informacion dada
+    const usuario = await Usuario.create(req.body)//Crea un nuevo usuario con la informacion dada
     res.json(usuario)// retorna con informacion bd
-}   
+}
 
 const formularioRecuperarPassword = (req, res) => {
     //funcion para representar las vistas
@@ -40,5 +43,5 @@ const formularioRecuperarPassword = (req, res) => {
 
 //export nombrado para multiples exportaciones.
 export {
-    formularioLogin, formularioRegistro, registrar,formularioRecuperarPassword
+    formularioLogin, formularioRegistro, registrar, formularioRecuperarPassword
 }
